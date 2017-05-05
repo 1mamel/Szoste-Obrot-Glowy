bbc3810b630c3272f8a5ada4cece50ffe9f6af9b
Kamil Pilch <mamlikk2@gmail.com>
2017-05-05 16:56:59 +0200

    first commit

---
+++ Assets/AnimOnRuntime.cs
@@ -0,0 +1,21 @@
+﻿// This script example shows how SetCurve() can be used
+using UnityEngine;
+using System.Collections;
+
+public class AnimOnRuntime : MonoBehaviour
+{
+    // Animate the position and color of the GameObject
+    public void Start()
+    {
+    }
+
+    public void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            if (!GetComponent<Animation>().isPlaying)
+            GetComponent<Animation>().Play();
+        }
+
+    }
+}
---
+++ Assets/TriggerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerController : MonoBehaviour {
+
+
+    private float timeLeft = 300000;
+    private Vector3 startScale;
+    private Collider collided;
+    private Color startColor;
+    public GameObject teleportToPlace;
+    
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Collider>().CompareTag("Player"))
+        {
+            collided = other;
+            timeLeft = 2;
+        }
+    }
+
+    void Start()
+    {
+        startScale = transform.localScale;
+        startColor = gameObject.GetComponent<Renderer>().material.color;
+
+    }
+    void Update()
+    {
+        transform.Rotate(new Vector3(0,0.5f,0));
+        timeLeft -= Time.deltaTime;
+        if (timeLeft < 2)
+        {
+            gameObject.GetComponent<Renderer>().material.color -= new Color(0,0,0,0.05f);
+            transform.localScale += new Vector3(0.05f, 0.05f, 0.05f);
+        }
+        if (timeLeft < 0)
+        {
+            collided.transform.position = teleportToPlace.transform.position + new Vector3(0,2,0);
+            transform.localScale = startScale;
+            gameObject.GetComponent<Renderer>().material.color = startColor;
+            timeLeft = 300000000;
+        }
+    }
+}
